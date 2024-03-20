4044a7cb775dc51046014ad4ff1e5466dc2f5ffb
Hosam <Hosamksbaa@gmail.com>
2024-03-20 15:04:59 +0200

    Z

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,186 +20,223 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Hosam Ksbaa",
+  title: "Hello, I'm Hosam",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A dedicated Software Engineer with a robust background in cross-platform app development, automation, big data, and machine learning. With a Bachelor's in Computer Science and a Master's in Informatics, I thrive on creating cutting-edge solutions that enhance human capabilities through technology. Expert in DevOps, cloud computing, and MLOps, I'm committed to building scalable, secure applications and deploying sophisticated machine learning models. Passionate about problem-solving, continuous learning, and collaboration."
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: true // Set false to hide this section, defaults to true
+  resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+  displayGreeting: true
 };
+
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/HosamKsbaa",
+  linkedin: "https://www.linkedin.com/in/hosamksbaa/",
+  gmail: "hosamksba.dev@gmail.com",
+  // gitlab: "https://gitlab.com/saadpasta",
+  // facebook: "https://www.facebook.com/saad.pasta7",
+  // medium: "https://medium.com/@saadpasta",
+  // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
 };
 
 // Skills Section
-
-const skillsSection = {
-  title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
-  skills: [
-    emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
-    ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
-    emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
-    )
-  ],
-
   /* Make Sure to include correct Font Awesome Classname to view your icon
 https://fontawesome.com/icons?d=gallery */
+const skillsSection = {
+  title: "What I Do",
+  subTitle: "DEVOPS & DATA SCIENCE ENTHUSIAST WHO LOVES TO EXPLORE AND MASTER EVERY TECH STACK",
+  skills: [
+    emoji("⚡ Develop interactive front-end user interfaces for web and mobile applications"),
+    emoji("⚡ Build and maintain scalable and efficient backend systems"),
+    emoji("⚡ Design and implement robust Big Data analytics solutions"),
+    emoji("⚡ Expert in cloud computing and infrastructure automation"),
+    emoji("⚡ Develop and deploy machine learning models for predictive analytics and AI-driven applications"),
+    emoji("⚡ Ensure high availability and fault tolerance using modern DevOps practices")
+  ],
 
   softwareSkills: [
-    {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
-    }
+    { skillName: "HTML5", fontAwesomeClassname: "fab fa-html5" },
+    { skillName: "CSS3", fontAwesomeClassname: "fab fa-css3-alt" },
+    { skillName: "Sass", fontAwesomeClassname: "fab fa-sass" },
+    { skillName: "JavaScript", fontAwesomeClassname: "fab fa-js" },
+    { skillName: "React", fontAwesomeClassname: "fab fa-react" },
+    { skillName: "Node.js", fontAwesomeClassname: "fab fa-node" },
+    { skillName: "Python", fontAwesomeClassname: "fab fa-python" },
+    { skillName: "Docker", fontAwesomeClassname: "fab fa-docker" },
+    { skillName: "AWS", fontAwesomeClassname: "fab fa-aws" },
+    { skillName: "Firebase", fontAwesomeClassname: "fas fa-fire" },
+    { skillName: "SQL Database", fontAwesomeClassname: "fas fa-database" },
+    { skillName: "MongoDB", fontAwesomeClassname: "fas fa-database" },
+    { skillName: "Apache Kafka", fontAwesomeClassname: "fas fa-stream" },
+    { skillName: "Neo4j", fontAwesomeClassname: "fas fa-project-diagram" },
+    { skillName: "NumPy", fontAwesomeClassname: "fas fa-square-root-alt" },
+    { skillName: "Pandas", fontAwesomeClassname: "fas fa-chart-bar" },
+    { skillName: "PySpark", fontAwesomeClassname: "fas fa-rocket" },
+    { skillName: "FastAPI", fontAwesomeClassname: "fas fa-server" },
+    { skillName: "Apache Spark", fontAwesomeClassname: "fas fa-bolt" },
+    { skillName: "Hadoop", fontAwesomeClassname: "fas fa-hdd" },
+    { skillName: "CircleCI", fontAwesomeClassname: "fas fa-circle" },
+    { skillName: "Jira", fontAwesomeClassname: "fab fa-jira" },
+    { skillName: "Linux", fontAwesomeClassname: "fab fa-linux" },
+    { skillName: "Flutter", fontAwesomeClassname: "fas fa-mobile-alt" }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: true
 };
+
 
 // Education Section
 
 const educationInfo = {
-  display: true, // Set false to hide this section, defaults to true
+  display: true, // Set to true to display this section
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "Nile University",
+      logo: require("./assets/images/nileUniversityLogo.png"), // Update with your actual logo path
+      subHeader: "M.Sc. and Professional Masters in Informatics, Informatics",
+      duration: "June 2022 - June 2024",
+      desc: "Pursuing M.Sc. in CIT-Informatics, focusing on a broad range of informatics science topics. Acquired skills in Image Processing, Computer Graphics, Scientific Computing, Statistical Analysis and Machine Learning, Introduction to Big Data, Advanced Big Data Analytics, and Data Mining of Massive Datasets.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Opportunity to delve into computing fundamentals with applications in high-growth CIT sectors.",
+        "Preparation for R&D careers in academia or industry through comprehensive coursework."
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "Ahram Canadian University - ACU",
+      logo: require("./assets/images/acuLogo.png"), // Update with your actual logo path
+      subHeader: "Bachelor of Computer Science \"Software Engineering\"",
+      duration: "2017 - August 2021",
+      desc: "Graduated with Honors; GPA: 3.83/4. Engaged in extracurricular activities such as NASA Space App, IEEE, Google Developer Student Clubs, and sports like Javelin Throw and Modern Pentathlon.",
+      descBullets: [
+        "Actively contributed to educational communities, assisting peers in academic pursuits.",
+        "Developed a diverse skill set and interests outside of academics through extensive involvement in clubs and societies."
+      ]
     }
   ]
 };
+
 
 // Your top 3 proficient stacks/tech experience
 
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
+  viewSkillBars: true, // Set it to true to show proficiency section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      Stack: "Big Data Analytics", // Area of expertise
+      progressPercentage: "95%" // Relative proficiency
     },
     {
-      Stack: "Backend",
-      progressPercentage: "70%"
+      Stack: "DevOps",
+      progressPercentage: "90%"
     },
     {
-      Stack: "Programming",
-      progressPercentage: "60%"
+      Stack: "Data Science & Machine Learning",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Cloud Computing (AWS)",
+      progressPercentage: "88%"
+    },
+    {
+      Stack: "Full Stack Development",
+      progressPercentage: "85%"
+    },
+    {
+      Stack: "Database Management (SQL/NoSQL)",
+      progressPercentage: "85%"
+    },
+    {
+      Stack: "Software Development (Best Practices, Design Patterns, Project Management)",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Cross-platform Development (Flutter for iOS/Android)",
+      progressPercentage: "80%"
     }
   ],
-  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+  displayCodersrank: false // Keep false as displaying Codersrank badges is not specified
 };
+
 
 // Work experience section
 
 const workExperiences = {
-  display: true, //Set it to true to show workExperiences Section
+  display: true, // Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      role: "Research and Teaching Assistant",
+      company: "Nile University",
+      companylogo: require("./assets/images/nileUniversityLogo.png"),
+      date: "October 2023 - Present",
+      desc: "Engaged in cutting-edge research and provided academic support in AI, DevOps, big data, and software engineering. Guided students through labs, coursework, and external projects, integrating practical applications with theoretical knowledge.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Contributed to research in AI, DevOps, and big data analytics.",
+        "Taught courses in software engineering, operating systems, and more."
       ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "DevOps Engineer",
+      company: "Ahram Canadian University - ACU",
+      companylogo: require("./assets/images/acuLogo.png"),
+      date: "June 2022 - October 2023",
+      desc: "Developed internal software tools, managed Moodle on AWS, and handled network administration and security. Implemented automation tools, enhancing operational efficiency and cloud infrastructure management.",
+      descBullets: [
+        "Managed and configured cloud-based tools and services on AWS.",
+        "Enhanced system security and efficiency through DevOps practices."
+      ]
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Software Developer",
+      company: "Freelance (Upwork, Mostaql)",
+      companylogo: require("./assets/images/freelanceLogo.png"), // Placeholder, replace with actual logo if available
+      date: "2016 - January 2023",
+      desc: "Developed and maintained Flutter applications for clients, from conceptualization to deployment, ensuring high-quality mobile app solutions.",
+      descBullets: [
+        "Specialized in Flutter for cross-platform mobile app development.",
+        "Managed full app lifecycle for various clients on Upwork and Mostaql."
+      ]
+    },
+    {
+      role: "Software Engineer",
+      company: "BlueBook-X",
+      companylogo: require("./assets/images/bluebookXLogo.png"), // Placeholder, replace with actual logo if available
+      date: "January 2020 - 2022",
+      desc: "Led client engagements, managed projects, and delivered tailored software solutions, ensuring client satisfaction and operational excellence.",
+      descBullets: [
+        "Oversaw project management and client relations in a software house setting.",
+        "Documented technical specifications and supported clients from onboarding to delivery."
+      ]
+    },
+    {
+      role: "Training Coordinator",
+      company: "Resala Charity Organization",
+      companylogo: require("./assets/images/resalaLogo.png"), // Placeholder, replace with actual logo if available
+      date: "2016 - 2018",
+      desc: "Developed and executed training programs, supporting educational initiatives for students and underprivileged individuals, enhancing community development and education access.",
+      descBullets: [
+        "Led team management and training program development.",
+        "Contributed to humanitarian efforts through educational initiatives."
+      ]
+    },
+    {
+      role: "Unity Game Developer",
+      company: "EGyDark (Self-employed)",
+      companylogo: require("./assets/images/egydarkLogo.png"), // Placeholder, replace with actual logo if available
+      date: "January 2014 - 2017",
+      desc: "Initiated career in game development, focusing on AI and game design, and contributed to indie game projects, fostering skills in programming and creative design.",
+      descBullets: [
+        "Developed AI for game objects and contributed to game design.",
+        "Pioneered in indie game development, enhancing programming and design skills."
+      ]
     }
   ]
 };
+
 
 /* Your Open Source Section to View Your Github Pinned Projects
 To know how to get github key look at readme.md */
